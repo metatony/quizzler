fd3baff6b4c1b9c33727f69e4a48a4a8878932e6
alexacolbe <58854674+alexacolbe@users.noreply.github.com>
2022-11-21 17:17:10 +0100

    added scorekeeper

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,139 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Quizzler());
-}
+void main() => runApp(const Quizzler());
 
-class Quizzler extends StatefulWidget {
+class Quizzler extends StatelessWidget {
   const Quizzler({super.key});
 
-  @override
-  State<Quizzler> createState() => _QuizzlerState();
-}
-
-class _QuizzlerState extends State<Quizzler> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(137, 31, 31, 31),
-        appBar: AppBar(
-          title: const Text('Quiz'),
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [],
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
         ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = const [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: MaterialButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    const Icon(Icons.check, color: Colors.green),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: MaterialButton(
+              color: Colors.red,
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                setState(() {
+                  scoreKeeper.add(
+                    const Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper, //scoreKeeper list
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
